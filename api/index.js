d58593ab4e02469da70abcb267dc2eb31e0a05e7
GovindSharma-392 <91676467+GovindSharma-392@users.noreply.github.com>
2024-07-24 17:31:57 +0530

    vercel config changes

--- api/index.js
+++ api/index.js
@@ -2,22 +2,20 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const dish = require('./routes/dish.js');
-const reviewRoutes = require('./routes/reviewRoutes.js')
+const dish = require('../routes/dish.js');
+const reviewRoutes = require('../routes/reviewRoutes.js')
 const methodOverride = require('method-override');
-const userRouter = require('./routes/user.js')
-const User = require('./models/userSchema.js');
+const userRouter = require('../routes/user.js')
+const User = require('../models/userSchema.js');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const flash = require('connect-flash');
 const session = require("express-session");
 
-
-
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 
 app.use(session({
   secret: 'keyboard cat',
@@ -41,18 +39,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(flash());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static('public'));
 
 app.use((req, res, next)=>{
-  // console.log(req.user);
   res.locals.currUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 })
 
-mongoose.connect("mongodb://127.0.0.1:27017/config").then(() => 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/config").then(() => 
     console.log('MongoDB Connected')
 )
   .catch(err => console.log(err));
@@ -61,9 +58,9 @@ app.use('/dish', dish);
 app.use('/review',reviewRoutes);
 app.use('/user', userRouter)
 
-
-
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+module.exports = app;
